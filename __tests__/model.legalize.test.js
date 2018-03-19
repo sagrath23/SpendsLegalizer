da43850131289576b30462dd05129b82d8fdc12f
Steven Villegas <sagrath23@gmail.com>
2018-03-19 18:15:31 -0500

    added more test cases to models/legalize.js

--- __tests__/model.legalize.test.js
+++ __tests__/model.legalize.test.js
@@ -2,17 +2,52 @@
 
 const legalizeDocuments = require("../models/legalize");
 
-test("Basic test", function(){ 
+test("Sum of bills is greater than sum of tickets", function(done){ 
     const basicInput = {
         bonos: [3,6,2],
         facturas: [5,1,7,3]
     };
     
-    const expectedOutput = ["1", " 1", " 1", " 0", " 1", " 1", " 1"]
+    const expectedOutput = ["1", " 1", " 1", " 0", " 1", " 1", " 1"];
 
     const promise = legalizeDocuments(basicInput.bonos, basicInput.facturas);
 
     promise.then((result) => {
         expect(result).toEqual(expectedOutput);
+        done();
+    });
+});
+
+test("Sum of tickets are equal to sum of bills", function(done){ 
+    const basicInput = {
+        bonos: [3,6,2],
+        facturas: [1,7,3]
+    };
+    
+    const expectedOutput = ["1", " 1", " 1", " 1", " 1", " 1"];
+
+    const promise = legalizeDocuments(basicInput.bonos, basicInput.facturas);
+
+    promise.then((result) => {
+        expect(result).toEqual(expectedOutput);
+        done();
+    });
+});
+
+test("Sum of tickets are greater than sum of bills", function(done){ 
+    const basicInput = {
+        bonos: [3,6,2,7],
+        facturas: [5,1,7,3]
+    };
+    
+    const expectedOutput = ["1", " 1", " 1", " 1", " 1", " 1", " 1", " 1"];
+
+    const promise = legalizeDocuments(basicInput.bonos, basicInput.facturas);
+
+    promise.then((result) => {
+        expect(result).toEqual(expectedOutput);
+        done();
+    }).catch((err) => {
+        done.fail(err);
     });
 });
