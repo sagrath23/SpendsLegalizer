db5371f9c9501197656d2b5fc19790b9b9501996
Steven Villegas <sagrath23@gmail.com>
2018-03-21 22:03:01 -0500

    All works OK

--- controllers/legalize.js
+++ controllers/legalize.js
@@ -3,21 +3,24 @@ var greedyLegalizeDocuments = require("../models/legalize");
 var legalizer = {};
 // controller that handles request to solve a rubik cube
 legalizer.legalize = function(req, res) {
+  var status;
 
-  if (!req.body.state) {
+  if (!req.body) {
     res.status(400);
-    res.send({
+    status =  {
       status: "error",
       error: "No Bills or Tickets sent to legalize."
-    });
+    }
   } else {
-    const objectResult = greedyLegalizeDocuments(req.body.tickets, req.body.bills);
+    const objectResult = greedyLegalizeDocuments(req.body.bonos, req.body.facturas);
 
-    res.send({
+    status = {
       status: "Ok",
       result: objectResult
-    });
+    }
   }
+
+  res.send(status);
 };
 
 legalizer.test = function(req, res) {
